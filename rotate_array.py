1b84ecd82d62665f78fab99f382e78630d2bdeaa
chefmohima <38052670+chefmohima@users.noreply.github.com>
2018-10-13 21:29:05 +0530

    Create rotate_array.py

---
+++ rotate_array.py
@@ -0,0 +1,16 @@
+def rotate(list,k):
+    if k > len(list):
+        k = k % len(list)
+    list.reverse()
+    list1 = list[:k]
+    list2 = list[k:]
+    
+    if len(list1) > 1:
+        list1.reverse()
+    if len(list2) > 1:
+        list2.reverse()
+    
+    list[:] = list1 + list2
+    return list
+    
+print(rotate([1,2,3,4],1))
