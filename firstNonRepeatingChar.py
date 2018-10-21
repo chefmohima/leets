18a39c122bff5f94cca010dbb196bb0a40b2d34a
chefmohima <38052670+chefmohima@users.noreply.github.com>
2018-10-21 20:45:40 +0530

    Create firstNonRepeatingChar.py

---
+++ firstNonRepeatingChar.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        s = list(s)
+        t = list(t)
+        s.sort()
+        t.sort()
+        
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                return False
+        return True
